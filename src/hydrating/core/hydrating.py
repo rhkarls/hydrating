2046ca46480c9bd536449a38aea281c1e12de075
Reinert Huseby Karlsen <2330659+rhkarls@users.noreply.github.com>
2026-05-23 13:49:31 +0200

    chore: update deps

--- src/hydrating/core/hydrating.py
+++ src/hydrating/core/hydrating.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from ..models import RatingModel
+from hydrating.models import RatingModel
 
 # from .grades import Grades
 
